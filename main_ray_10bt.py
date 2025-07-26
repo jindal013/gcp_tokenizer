af2d487979b59fa9619037d0173e0deb903fe8e9
Chinmay <chinmayjindal108@gmail.com>
2025-07-26 18:05:57 -0400

    distributed upload via ray

--- main_ray_10bt.py
+++ main_ray_10bt.py
@@ -1,48 +1,63 @@
-"""
-This script automatically streams the sample-10BT FineWeb-EDU dataset and uploads to desired GCP bucket using multiprocessing. 
-"""
-
 import os
 import multiprocessing as mp
 import numpy as np
 import tiktoken
-from datasets import load_dataset # pip install datasets
-from tqdm import tqdm # pip install tqdm
+from datasets import load_dataset
+from tqdm import tqdm
 from google.cloud.storage import Client, transfer_manager
 import ray
+import time
+import socket
+from collections import Counter
 from ray.util.multiprocessing import Pool
 
 BLUE = '\033[34m'
 RESET = '\033[0m'
 
 ray.init() # for main node
+assert ray.is_initialized()
+
+# checking if ray works before script
+
+print('''This cluster consists of
+    {} nodes in total
+    {} CPU resources in total
+'''.format(len(ray.nodes()), ray.cluster_resources()['CPU']))
+
+@ray.remote
+def f():
+    time.sleep(0.001)
+    return socket.gethostbyname("localhost")
+
+object_ids = [f.remote() for _ in range(10000)]
+ip_addresses = ray.get(object_ids)
+
+print('Tasks executed')
+for ip_address, num_tasks in Counter(ip_addresses).items():
+    print('    {} tasks on {}'.format(num_tasks, ip_address))
 
 # ------------------------------------------
+# code for script
+
 local_dir = "edu_fineweb10B"
 remote_name = "sample-10BT"
-shard_size = int(1e8) # 100M tokens per shard, total of 100 shards
+shard_size = int(1e8)
 
 BUCKET_NAME = "test2_10bt_gpt4"
-# DATA_CACHE_DIR = "/home/chinmay/data_jax/edu_fineweb10B"
 WORKERS = int(os.cpu_count() / 1.5)
-
-# create the cache the local directory if it doesn't exist yet
 DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
 FILE_NAMES = os.listdir(DATA_CACHE_DIR)
 os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 
-# download the dataset
 fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train", streaming=True)
 
-# init the tokenizer
 enc = tiktoken.encoding_for_model("gpt-4") # 'cl100k_base'
 
-eot = enc._special_tokens['<|endoftext|>'] # end of text token
+eot = enc._special_tokens['<|endoftext|>'] 
 
 @ray.remote
 def tokenize(doc):
-  # tokenizes a single document and returns a numpy array of uint32 tokens
-  tokens = [eot] # the special <|endoftext|> token delimits all documents
+  tokens = [eot]
   tokens.extend(enc.encode_ordinary(doc["text"]))
   tokens_np = np.array(tokens)
   assert (0 <= tokens_np).all() and (tokens_np < 2**32).all(), "token dictionary too large for uint32"
@@ -56,9 +71,7 @@ def write_datafile(filename, tokens_np):
 print(f'{BLUE}hf dataset has been accessed {RESET}') # not downloaded now as we are streaming the dataset
 
 
-# tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
 cpu_count = os.cpu_count()
-# nprocs = max(1, int(cpu_count / 1.5))
 
 storage_client = Client()
 bucket = storage_client.bucket(BUCKET_NAME)
@@ -83,7 +96,7 @@ def upload_file():
   FILE_NAMES = os.listdir(DATA_CACHE_DIR)
   print('------------')
   print(f'starting upload to GCP bucket {BUCKET_NAME}')
-  upload_many_blobs_with_transfer_manager(BUCKET_NAME, FILE_NAMES, DATA_CACHE_DIR, WORKERS)
+  upload_many_blobs_with_transfer_manager.remote(BUCKET_NAME, FILE_NAMES, DATA_CACHE_DIR, WORKERS)
   print(f'done upload to GCP bucket {BUCKET_NAME} for {FILE_NAMES}')
   print('cleaning up files....')
   for file in FILE_NAMES:
@@ -94,19 +107,13 @@ def upload_file():
 # def main():
 with Pool(ray_address="auto") as pool:
   shard_index = 0
-  # preallocate buffer to hold current shard
   all_tokens_np = np.empty((shard_size,), dtype=np.uint32)
   token_count = 0
   progress_bar = None
   
-  for tokens in pool.map(tokenize, fw, chunksize=16):
+  for tokens in pool.map(tokenize.remote, fw, chunksize=32):
 
-    # print(tokens)
-    # break
-
-    # is there enough space in the current shard for the new tokens?
     if token_count + len(tokens) < shard_size:
-      # simply append tokens to current shard
       all_tokens_np[token_count:token_count+len(tokens)] = tokens
       token_count += len(tokens)
       # update progress bar
@@ -121,8 +128,8 @@ with Pool(ray_address="auto") as pool:
       remainder = shard_size - token_count
       progress_bar.update(remainder)
       all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
-      write_datafile(filename, all_tokens_np)
-      upload_file()
+      write_datafile.remote(filename, all_tokens_np)
+      upload_file.remote()
       shard_index += 1
       progress_bar = None
       # populate the next shard with the leftovers of the current doc
@@ -133,8 +140,8 @@ with Pool(ray_address="auto") as pool:
   if token_count != 0:
     split = "val" if shard_index == 0 else "train"
     filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
-    write_datafile(filename, all_tokens_np[:token_count])
-    upload_file()
+    write_datafile.remote(filename, all_tokens_np[:token_count])
+    upload_file.remote()
 
 # if __name__ == '__main__':
 #   main()
